4e0dde3c390653cbf6a60ac9d8f2c69855a0708e
yahya <101sayyid@gmail.com>
2026-04-25 15:30:32 +0100

    fixing header in cart page

--- cart.js
+++ cart.js
@@ -82,3 +82,16 @@ clearBtn.addEventListener("click", () => {
 
 // INIT
 displayCart();
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
